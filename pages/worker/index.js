a4aab219a942e706503f812c03f70e373cd4e4e3
arturkarpinets77-ux <arturkarpinets77@gmail.com>
2026-03-05 14:31:17 +0200

    Update index.js

--- pages/worker/index.js
+++ pages/worker/index.js
@@ -132,9 +132,9 @@ export default function WorkerDashboard() {
         </div>
 
         <div className={styles.actionsGrid}>
-  <Link href="/workday" legacyBehavior>
-    <a className={styles.actionButton}>Отметка рабочего дня</a>
-  </Link>
+  <Link href="/worker/workday" className={styles.actionButton}>
+  Отметка рабочего дня
+</Link>
 
   <Link href="/worker/photo" legacyBehavior>
     <a className={styles.actionButton}>Добавить фотоотчёт</a>
